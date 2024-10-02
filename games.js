c4cd30ccc8d2e4431e8177af87577976020e9467
web2098 <7141138+web2098@users.noreply.github.com>
2024-10-02 16:26:23 -0600

    Update games.js (#33)

--- games.js
+++ games.js
@@ -271,7 +271,7 @@ function Survivor(){
         "board": [
         ],
         "free_space_dynamic": false,
-        "free_space_on": true,
+        "free_space_on": false,
         "transitional": false,
         "rules": "Stand up with both boards up. If a number on a board gets called flip the board. If both boards are flipped sit down. Last person standing wins!",
         "length": 1
