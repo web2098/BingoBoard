bab2eda54afa8b39e16873f87a4fb9397b979db1
eric-gressman <eric.gressman@hp.com>
2025-07-15 15:12:17 -0600

    More attempts

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,23 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import React from 'react';
+
+// Mock Swiper components for testing
+jest.mock('swiper/react', () => ({
+  Swiper: ({ children, ...props }: any) => React.createElement('div', { 'data-testid': 'swiper', ...props }, children),
+  SwiperSlide: ({ children, ...props }: any) => React.createElement('div', { 'data-testid': 'swiper-slide', ...props }, children),
+}));
+
+jest.mock('swiper/modules', () => ({
+  Navigation: {},
+  Pagination: {},
+  Mousewheel: {},
+  Keyboard: {},
+}));
+
+// Mock Swiper CSS imports
+jest.mock('swiper/css', () => {});
+jest.mock('swiper/css/navigation', () => {});
+jest.mock('swiper/css/pagination', () => {});
+jest.mock('swiper/css/mousewheel', () => {});
